9813265b71fc49d82be2e81bb85edce0d2cc54d0
Juank_Valero <jvalero2009@gmail.com>
2024-11-18 20:51:41 -0500

    feat: Add logo components and remove SoftSkillsPage

---
+++ client/src/presentation/icons/logos/index.ts
@@ -0,0 +1,5 @@
+export * from './Css.logo';
+export * from './Express.logo';
+export * from './Html.logo';
+export * from './Javascript.logo';
+export * from './Typescript.logo';
--- client/src/presentation/pages/skills/TechSkillsPage.tsx
+++ client/src/presentation/pages/skills/TechSkillsPage.tsx
@@ -1,8 +1,30 @@
+import { CssLogo, HtmlLogo, JavascriptLogo, TypescriptLogo } from '../../icons';
 
 export const TechSkillsPage = () => {
-  return (
-    <div className='fade-in text-3xl font-bold underline'>
-      Habilidades técnicas
-    </div>
-  )
+    return (
+        <div className="w-full h-full flex flex-col">
+            <div className="fade-in text-3xl font-bold underline">
+                Habilidades técnicas
+            </div>
+            <div className='w-full flex flex-col justify-center items-center my-4'>
+            <div className="fade-in text-2xl font-bold my-4 text-center">
+                Lenguajes de programación
+            </div>
+                <div className="grid grid-cols-4 gap-4 w-3/4 pl-8">
+                    <div className="w-40 h-40 flex items-center justify-center">
+                        <JavascriptLogo />
+                    </div>
+                    <div className="w-40 h-40  flex items-center justify-center">
+                        <TypescriptLogo />
+                    </div>
+                    <div className="w-40 h-40  flex items-center justify-center">
+                        <HtmlLogo />
+                    </div>
+                    <div className="w-40 h-40  flex items-center justify-center">
+                        <CssLogo />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
